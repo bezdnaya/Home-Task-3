411b6db432f7b039f7eb03e6055873d0f00c457f
Irina <bezdnaya@mail.ru>
2022-11-28 16:50:47 +0200

    Task 23 Нахождение кубов от 1 до N

---
+++ Task23_Cubes/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите целое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+Console.WriteLine("Кубы от 1 до N =");
+while (index <= N)
+{
+    Console.Write($"{Math.Pow(index, 3)} ");
+    index++;
+}
